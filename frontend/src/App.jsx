db5d1a0483b222f0adc106ccc8e94c97c4b6614b
Prasad <prasad.gujar2004@gmail.com>
2024-04-12 04:36:11 +0530

    CompanyDashboard_updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,11 +10,13 @@ import CoordinatorLogin from './Components/CoordinatorLogin'
 import CompanyLanding from './pages/CompanyDashboard'
 import CompanyRegister from './Components/CompanyRegister'
 import StudentRegister from './Components/StudentRegister'
-
+import ActiveJobOpenings from './pages/ActiveJobOpenings'
+import { useState } from 'react'
 
 
 
 function App() {
+  const [jobPosts, setJobPosts] = useState([]);
   return (
     <>
       <BrowserRouter>
@@ -22,12 +24,20 @@ function App() {
           <Route path='/' element={<LandingPage />}></Route>
           <Route path='/signin' element={<SignIn/>} ></Route>
           <Route path='/company' element={<CompanyLogin/>} ></Route>
-          <Route path='/companyhome' element={<CompanyLanding/>} ></Route>
+          {/* <Route path='/companyhome' element={<CompanyLanding/>} ></Route> */}
+          <Route path="/companyhome" element={
+                    <CompanyLanding jobPosts={jobPosts} setJobPosts={setJobPosts} />
+                } />
           <Route path='/register/company' element={<CompanyRegister/>} ></Route>
           <Route path='/register/student' element={<StudentRegister/>} ></Route>
           {/* <Route path='/signin?company' element={<SignIn/>} ></Route> */}
           <Route path='/coordinator' element={<CoordinatorLogin/>} ></Route>
           <Route path='/dev' element={<StickyScrollRevealDemo/>}></Route>
+          {/* <Route path='/activejobopenings' element={<ActiveJobOpenings/>}></Route> */}
+          {/* <Route path="/companyhome" exact>
+                <CompanyLanding jobPosts={jobPosts} setJobPosts={setJobPosts} />
+            </Route> */}
+            <Route path='/activejobopenings' element={<ActiveJobOpenings jobPosts={jobPosts} setJobPosts={setJobPosts} />} />
         </Routes>
         
       </BrowserRouter>
